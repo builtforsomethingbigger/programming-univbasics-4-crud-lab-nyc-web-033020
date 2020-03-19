bf03fc339fa7554fcd3fa697295c07df380befac
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-19 23:06:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  new_array = ["Mom", "Dad", "Brother", "Sister"]
+  new_array << "Baby"
 end
 
 def add_element_to_start_of_array(array, element)
