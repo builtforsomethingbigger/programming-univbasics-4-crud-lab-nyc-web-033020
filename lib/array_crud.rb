7e81a3de1d007cadbb46814ed7604d940636ff7c
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-19 23:06:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   new_array = ["Mom", "Dad", "Brother", "Sister"]
-  new_array << "Baby"
+  new_array.push "Baby"
 end
 
 def add_element_to_start_of_array(array, element)
