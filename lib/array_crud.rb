7c537c94dd3ef71ef674bb6ac24335d7874068c3
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-19 23:16:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,6 +35,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  index_number = array[4]
-  return index_number
+  array[4]
+  return array
 end
